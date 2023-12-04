7e849e6b17cbf334bff58c39159a687de90a7726
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-12-04 13:17:04 -0800

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):str.find("object-oriented programming") + len("object-oriented programming")], end="\n")
+str = "Python is an interpreted, interactive, object-oriented programming with python language that combines remarkable power with very clear syntax"
+print(str[str.find("object-oriented programming with python"):str.find("object-oriented programming with python") + len("object-oriented programming with python")], end="\n")
