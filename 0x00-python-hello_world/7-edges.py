4c0a86201b0dfac402609ce6efe98afc3d8e4baa
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-12-06 12:28:19 -0800

    update

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 =  word[6:8]
-middle_word =  word[3:6]
-print(f"{word_first_3}")
-print(f"{word_last_2}")
-print(f"{middle_word}")
+word_last_2 =  word[7:9]
+middle_word =  word[1:8]
+print("First 3 letters:"f"{word_first_3}")
+print("Last 2 letters:"f"{word_last_2}")
+print("Middle word:"f"{middle_word}")
