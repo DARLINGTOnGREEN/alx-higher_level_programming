ef01f89b1703c5d1ab65fc31f4aabe6406eba714
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-04 09:27:14 +0100

    upload

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """Empty class that defines a square"""
+    """
+    This class defines a square.
+
+    Attributes:
+        side_length (int): The length of a side of the square.
+
+    Methods:
+        area(): Returns the area of the square.
+    """
     pass
