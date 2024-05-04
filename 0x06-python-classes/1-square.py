5c7c5379d5dea19c2485181cf09d3acd4601796a
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-04 09:06:06 +0100

    upload

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    '''defines a square'''
+    """defines a square"""
     def __init__(self, size):
-        '''initialize size'''
+        """initialize size"""
         self.__size = size
