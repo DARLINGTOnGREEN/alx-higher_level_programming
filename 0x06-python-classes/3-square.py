58f0c444ae3bb91b2981f6970fb460f218965216
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-05 17:22:43 +0100

    upload

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,10 +6,12 @@ class Square:
     """Define an area of a square"""
 
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be aj integer")
-        elif type(size) < 0:
-            raise ValueError("size must be >=0")
+        """initailise the data"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
