043e5b3222b48649d40b6db8a63d60629795e622
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-13 20:38:50 +0100

    upload

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""define a class called magiC class"""
+class MagicClass:
+    def __init__(self, radius=0):
+        self._MagicClass__radius = 0
+
+    def area(self):
+        return self._MagicClass__radius ** 2
+
+    def circumference(self):
+        return 2 * 3.141592653589793 * self._MagicClass__radius
+
