635d222b41504af5c7b2adac5617d5157b88920a
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-18 19:09:57 +0100

    upload

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""function that prints a text with 2 new \
-        lines after each of these characters: ., ? and :"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """check if the text is a string"""
-
+    """text indent"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delimeter in "?:."
+    for delimeter in "?:.":
         words = (delimeter + "\n\n").join(
                 [index.strip(" ") for index in words.split(delimeter)])
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/5-text_indentation.txt")
