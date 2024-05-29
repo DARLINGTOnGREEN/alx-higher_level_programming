9500231bb61f632b6ce745c60626fbb5aff8c978
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-29 15:46:02 +0100

    upload

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module that defines a class"""
+
+
+class Rectangle:
+    """A class called Rectangle"""
+
+    def __init__(self, width=0, height=0):
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
