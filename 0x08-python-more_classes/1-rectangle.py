d0694bdea6619e22b1044f3ffb041d4104b51574
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-05-29 16:07:31 +0100

    upload

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,14 +7,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """getter"""
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -29,7 +28,6 @@ class Rectangle:
     def height(self):
         """getter"""
         return self.__height
-
 
     @height.setter
     def height(self, value):
