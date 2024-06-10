fd8c9aee68a8751685433b785fd607ada690e5b9
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-06-10 18:08:51 +0100

    upload

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
-"""Defines a class called node."""
+"""Defines a class called Node."""
 
 
 class Node:
-    """Defines a node of singly linked list"""
+    """Defines a node of a singly linked list."""
 
     def __init__(self, data, next_node=None):
-        """initialize the data and next_node"""
+        """Initialize the data and next_node attributes."""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """getter for data"""
+        """Get the data of the node."""
         return self.__data
 
     @data.setter
     def data(self, value):
-        """setter for data"""
+        """Set the data of the node."""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """getter for next_node"""
+        """Get the next node."""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """setter for next_node"""
+        """Set the next node."""
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """defines a singly linked list"""
+    """Defines a singly linked list."""
 
     def __init__(self):
-        """initialize singly linked list"""
+        """Initialize the singly linked list."""
         self.head = None
 
     def __str__(self):
-        """Return a string representation of the singly linked list"""
+        """Return a string representation of the singly linked list."""
         result = ""
         current = self.head
         while current:
@@ -52,14 +52,14 @@ class SinglyLinkedList:
         return result.rstrip("\n")
 
     def sorted_insert(self, value):
-        """insert a new Node into the correct sorted position"""
-    new_node = Node(value)
-    if not self.head or value < self.head.data:
-        new_node.next_node = self.head
-        self.head = new_node
-    else:
-        current = self.head
-        while current.next_node and current.next_node.data < value:
-            current = current.next_node
-        new_node.next_node = current.next_node
-        current.next_node = new_node
+        """Insert a new Node into the correct sorted position."""
+        new_node = Node(value)
+        if not self.head or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
