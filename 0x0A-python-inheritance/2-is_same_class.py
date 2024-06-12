6eb566219d4bdbac8b64f6d965f48df53cb9a717
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-06-12 14:11:30 +0100

    up

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that define a function"""
+
+
+def is_same_class(obj, a_class):
+    """function called is_same_class(obj, a_class)
+    return true if obj is a class else
+    false
+    """
+    return True if type(obj) is a_class else False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that defines a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of a class
+    otherwise False.
+    """
+    return isinstance(obj, a_class)
