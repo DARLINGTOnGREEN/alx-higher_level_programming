9b5b2ef88b272694601b638a53b0ff1eb0c4c968
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-06-19 20:47:29 +0100

    upload

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,13 +12,13 @@ class Student:
     -age
     """
 
-    def _init_(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initialize the student with first name, last name, and age."""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+    
     def to_json(self):
         """Retrives a dictionary representation
         of student instance
