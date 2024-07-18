3451b09cf13e5f976bd642ac9764abeda7eaef01
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-07-18 21:40:27 +0100

    upload

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-//script that prints two arguments and
-//passed to it, in the following format: “ is ”
+//script that prints two arguments
 
 console.log(process.argv[2] + ' is ' + process.argv[3]);
