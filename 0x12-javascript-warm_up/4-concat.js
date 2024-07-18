7ad651a3a1881c560ff0b6cbc92b5292d1b9aa27
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-07-18 21:42:21 +0100

    upload

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//script that prints two arguments
+// script that prints two arguments
 
 console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that prints My number: <first argument converted in integer>
+// script that prints My number: <first argument converted in integer>
 
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
