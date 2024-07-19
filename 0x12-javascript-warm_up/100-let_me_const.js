0f5ebb907b0337d0abee606e9b99d8b4bc96109b
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-07-20 00:40:42 +0100

    up

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// change value of myVar to 333
+
+maVar = 333;
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+// function that returns the addition of 2 integers.
+
+export.add = function (a, b) {
+  return a + b;
+};
