ca9cd05c5dcefd5d43fd283d0b0b8c456b749ade
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-07-19 23:59:48 +0100

    up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints x times “C is fun" 
 
-let lang = 'c is fun';
+let lang = 'C is fun';
 
 if (isNaN(process.argv[2])) {
 	console.log('Missing number of occurrences')
