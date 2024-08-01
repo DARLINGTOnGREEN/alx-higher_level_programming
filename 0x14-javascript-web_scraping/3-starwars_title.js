421a5b1da2946954e8e5a9016b919d110c1a6e78
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-08-01 21:52:31 +0100

    uoload

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints the title of a Star Wars
+
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
