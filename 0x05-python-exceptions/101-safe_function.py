6a3d026de07a61e0f433c81170e6576483576f2c
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-08-07 14:00:01 +0100

    up

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except (TypeError, ValueError, ZeroDivisionError, IndexError):
-        print("Exceptions: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as err:
+        print("Exceptions: {}".format(err), file=sys.stderr)
+        return None
